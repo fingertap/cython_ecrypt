7f7c216670b6b34d1a62ea390001202f1d8040c2
Han <zh950713@gmail.com>
2021-04-21 15:43:52 +0800

    add script, torch, setup

--- cython_encrypt/enc_module/enc_torch.py
+++ cython_encrypt/enc_module/enc_torch.py
@@ -6,7 +6,18 @@ from torchvision import models as tv_models
 class MyModel(nn.Module):
     def __init__(self):
         nn.Module.__init__(self)
-        self.resnet = tv_models.resnet18()
+        resnet = tv_models.resnet18()
+        self.model = nn.Sequential(
+            resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool,
+            resnet.layer1, resnet.layer2, resnet.layer3, resnet.layer4
+        )
 
     def forward(self, data):
-        return self.resnet(data)
+        return self.model(data)
+
+
+def test():
+    model = MyModel()
+    data = torch.rand(1, 3, 64, 64)
+    assert model(data).size() == (1, 512, 2, 2)
+    return 'OK'
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+
+setup(
+    ext_modules=cythonize([
+        'cython_encrypt/*.py',
+        'cython_encrypt/enc_module/*.py'
+    ])
+)
